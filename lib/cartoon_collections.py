486ae657f5e1499e87d9ddbe32f128118b0cc0c6
Elisa Shapiro <elisashapiro@gmail.com>
2023-10-05 12:58:22 -0400

    tests passed

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list_of_dwarves):
+    i = 1
+    for dwarf in list_of_dwarves:
+        print(f'{i}. {dwarf}')
+        i += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(list_of_calls):
+    return[f'{call.title()}!' for call in list_of_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(list_of_calls):
+    for call in list_of_calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+CHEESES = ["camembert", "cheddar", "gouda"]
+
+def find_the_cheese(list_of_foods):
+    for food in list_of_foods:
+        if food in CHEESES:
+            return food
+    return None
